9ab0424bcea9bbd3de6f01112dec6745f741f62e
nhanhem2ui <nhanhem2ui@gmail.com>
2025-10-18 03:37:01 +0000

    gg

--- Battleship/Controllers/HomeController.cs
+++ Battleship/Controllers/HomeController.cs
@@ -23,6 +23,13 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Reset()
+    {
+        HttpContext.Session.Remove("_Game");
+        HttpContext.Session.Remove("_TwoPlayerGame");
+        return RedirectToAction("Index");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
